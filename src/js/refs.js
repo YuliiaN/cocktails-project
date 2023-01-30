e3894c3ecfd950d1041d1867e313d17ec330ef93
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-01-30 02:04:21 +0200

    add files

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  logo: document.querySelector('.header__logo'),
+  burgerOpen: document.querySelector('button[data-burger-open]'),
+  burgerMenu: document.querySelector('[data-burger]'),
+  burgerClose: document.querySelector('button[data-burger-close]'),
+  headerSearch: document.querySelector('.header__search-wrapper'),
+};
