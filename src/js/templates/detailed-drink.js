dc1bb8587175ed38d6601de36fa42b2cb1109c89
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-01-31 00:05:35 +0200

    add cocktail-modal all devices

--- src/js/templates/detailed-drink.js
+++ src/js/templates/detailed-drink.js
@@ -20,25 +20,31 @@ export default function renderDetailedDrink({
   }
 
   return `
+      <div class="first-content-wrapper">
         <h2 class="cocktail-modal__title">${strDrink}</h2>
-      <h3 class="cocktail-modal__subtitle cocktail-modal__subtitle--first">
-        Instructions:
-      </h3>
-      <p class="cocktail-modal__description">
-        ${strInstructions}
-      </p>
-      <img src="${strDrinkThumb}" alt="${strDrink}" class="cocktail-modal__picture" />
-      <h3 class="cocktail-modal__subtitle cocktail-modal__subtitle--second">
-        INGREDIENTS
-      </h3>
-      <p class="cocktail-modal__ingredients">Per cocktail</p>
-      <ul class="cocktail-modal__list">
-        <li class="cocktail-modal__item"><span>✶</span> ${strIngredient1}</li>
-        <li class="cocktail-modal__item"><span>✶</span> ${strIngredient2}</li>
-        <li class="cocktail-modal__item"><span>✶</span> ${strIngredient3}</li>
-        <li class="cocktail-modal__item"><span>✶</span> ${strIngredient4}</li>
-        <li class="cocktail-modal__item"><span>✶</span> ${strIngredient5}</li>
-      </ul>
-      <button type="button" class="btn-add-fav">Add to favorite</button>
+        <h3 class="cocktail-modal__subtitle cocktail-modal__subtitle--first">
+          Instructions:
+        </h3>
+        <p class="cocktail-modal__description">
+          ${strInstructions}
+        </p>
+      </div>
+      <div class="second-content-wrapper">
+        <img src="${strDrinkThumb}" alt="${strDrink}" class="cocktail-modal__picture" />
+        <div class="third-content-wrapper">
+          <h2 class="cocktail-modal__title-tab">${strDrink}</h2>
+          <h3 class="cocktail-modal__subtitle cocktail-modal__subtitle--second">
+            INGREDIENTS
+          </h3>
+          <p class="cocktail-modal__ingredients">Per cocktail</p>
+          <ul class="cocktail-modal__list">
+            <li class="cocktail-modal__item"><span>✶</span> ${strIngredient1}</li>
+            <li class="cocktail-modal__item"><span>✶</span> ${strIngredient2}</li>
+            <li class="cocktail-modal__item"><span>✶</span> ${strIngredient3}</li>
+            <li class="cocktail-modal__item"><span>✶</span> ${strIngredient4}</li>
+            <li class="cocktail-modal__item"><span>✶</span> ${strIngredient5}</li>
+          </ul>
+        </div>
+      </div>
     `;
 }
