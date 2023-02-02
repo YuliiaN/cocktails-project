4678203235f687e5577e85ac06b4845e39a50694
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-02 03:35:00 +0200

    divide header styles

--- src/fav-cocktails.js
+++ src/fav-cocktails.js
@@ -3,5 +3,6 @@ import './js/modals/burger-menu';
 import './js/cocktail-api';
 import './js/query-gallery';
 import './js/modals/cocktail-modal';
+import './js/add-local-storage';
 import './js/render-local-storage';
 import './js/change-theme';
