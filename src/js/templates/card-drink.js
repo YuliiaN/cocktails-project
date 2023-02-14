ffe16be368db91b559e409bfa1782eb2b9d664c1
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-15 01:55:20 +0200

    add styles for liked buttons

--- src/js/templates/card-drink.js
+++ src/js/templates/card-drink.js
@@ -1,9 +1,13 @@
 import heart from '../../images/sprite.svg';
+import { STORAGE_KEY } from '../add-local-storage';
+import { buttonLike, buttonUnlike } from '../add-local-storage';
 
 export default function renderCardDrink(arr) {
+  const savedCocktails = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
   const collection = arr.map(({ idDrink, strDrink, strDrinkThumb }) => {
-    return `
-    <li class="gallery__item" id="${idDrink}">
+    if (savedCocktails.includes(idDrink)) {
+      return `<li class="gallery__item" id="${idDrink}">
   <img src="${strDrinkThumb}" alt="${strDrink}" class="gallery__item-picture" />
   <h2 class="gallery__item-title">${strDrink}</h2>
   <div class="gallery__buttons">
@@ -11,14 +15,24 @@ export default function renderCardDrink(arr) {
       Learn More
     </button>
     <button type="button" class="btn-status btn-ls">
-      Add to
-      <svg class="gallery__btn-icon heart">
-        <use href="${heart}#icon-heart" class="heart-icon">
-</use>
-      </svg>
+      ${buttonUnlike}
     </button>
   </div>
 </li>`;
+    } else {
+      return `<li class="gallery__item" id="${idDrink}">
+  <img src="${strDrinkThumb}" alt="${strDrink}" class="gallery__item-picture" />
+  <h2 class="gallery__item-title">${strDrink}</h2>
+  <div class="gallery__buttons">
+    <button type="button" class="btn-info" data-cocktail-modal-open>
+      Learn More
+    </button>
+    <button type="button" class="btn-status btn-ls">
+      ${buttonLike}
+    </button>
+  </div>
+</li>`;
+    }
   });
   return collection;
 }
