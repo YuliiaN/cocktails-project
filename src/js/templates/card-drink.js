d944db244386d3202fbaa9fa402338ae9f3ee587
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-16 17:28:50 +0200

    fix api request bug

--- src/js/templates/card-drink.js
+++ src/js/templates/card-drink.js
@@ -3,35 +3,35 @@ import { STORAGE_KEY } from '../add-local-storage';
 import { buttonLike, buttonUnlike } from '../add-local-storage';
 
 export default function renderCardDrink(arr) {
-  const savedCocktails = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const savedCocktails = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
 
   const collection = arr.map(({ idDrink, strDrink, strDrinkThumb }) => {
     if (savedCocktails.includes(idDrink)) {
-      return `<li class="gallery__item" id="${idDrink}">
+      return `
+<li class="gallery__item" id="${idDrink}">
   <img src="${strDrinkThumb}" alt="${strDrink}" class="gallery__item-picture" />
   <h2 class="gallery__item-title">${strDrink}</h2>
   <div class="gallery__buttons">
     <button type="button" class="btn-info" data-cocktail-modal-open>
       Learn More
     </button>
-    <button type="button" class="btn-status btn-ls">
-      ${buttonUnlike}
-    </button>
+    <button type="button" class="btn-status btn-ls">${buttonUnlike}</button>
   </div>
-</li>`;
+</li>
+`;
     } else {
-      return `<li class="gallery__item" id="${idDrink}">
+      return `
+<li class="gallery__item" id="${idDrink}">
   <img src="${strDrinkThumb}" alt="${strDrink}" class="gallery__item-picture" />
   <h2 class="gallery__item-title">${strDrink}</h2>
   <div class="gallery__buttons">
     <button type="button" class="btn-info" data-cocktail-modal-open>
       Learn More
     </button>
-    <button type="button" class="btn-status btn-ls">
-      ${buttonLike}
-    </button>
+    <button type="button" class="btn-status btn-ls">${buttonLike}</button>
   </div>
-</li>`;
+</li>
+`;
     }
   });
   return collection;
